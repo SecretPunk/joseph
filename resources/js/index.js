f0466f4408ec834ab86c34db9c50bda5fb378dac
SecretPunk <amanjit.gill1@gmail.com>
2019-07-24 13:05:23 +1000

    Initial commit.

---
+++ resources/js/index.js
@@ -0,0 +1,31 @@
+const VALIDLETTERS = "JOSEPHjoseph";
+const SOUNDPATH = "resources/sounds/";
+
+document.addEventListener("keydown", handleKey);
+
+function handleKey(event) {
+
+  var pressedKey = event.key;
+
+  if (VALIDLETTERS.includes(pressedKey)) {
+    var letter = pressedKey.toUpperCase();
+    animateLetter(letter);
+    playSound(letter);
+  }
+}
+
+function animateLetter(letter) {
+
+  var letterElement = document.querySelector("." + letter);
+
+  letterElement.classList.add("pressed");
+
+  setTimeout(function() {
+    letterElement.classList.remove("pressed");
+  }, 1200);
+}
+
+function playSound(letter) {
+
+  new Audio(SOUNDPATH + letter + ".mp3").play();
+}
